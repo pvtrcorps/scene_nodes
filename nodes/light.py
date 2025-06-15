6402351b0dfcd0d81433461d82ab483156887f4e
pvtrcorps <antoniojaviertorralbamoreno@gmail.com>
2025-06-15 12:51:12 +0200

    Add helper for node properties and refactor

--- nodes/light.py
+++ nodes/light.py
@@ -1,31 +1,39 @@
 import bpy
-from .base import (
-    BaseNode,
-    SceneSocket,
-    FloatSocket,
-    StringSocket,
-    VectorSocket,
-)
+from .base import BaseNode, build_props_and_sockets
 
 class LightNode(BaseNode):
     bl_idname = "LightNodeType"
     bl_label = "Light"
 
-    light_type: bpy.props.EnumProperty(
-        items=[('POINT','Point',''),('SUN','Sun',''),('SPOT','Spot',''),('AREA','Area','')],
-        name="Type"
-    )
-    energy: bpy.props.FloatProperty(name="Energy", default=10.0)
-    color: bpy.props.FloatVectorProperty(name="Color", subtype='COLOR', default=(1,1,1))
-
     def init(self, context):
-        t = self.inputs.new('StringSocketType', "Type")
-        t.value = self.light_type
-        e = self.inputs.new('FloatSocketType', "Energy")
-        e.value = self.energy
-        c = self.inputs.new('VectorSocketType', "Color")
-        c.value = self.color
+        self.add_property_sockets()
         self.outputs.new('SceneSocketType', "Scene")
 
     def draw_buttons(self, context, layout):
         pass  # Inputs already expose editable values next to their sockets
+
+
+build_props_and_sockets(
+    LightNode,
+    [
+        (
+            "light_type",
+            "enum",
+            {
+                "items": [
+                    ("POINT", "Point", ""),
+                    ("SUN", "Sun", ""),
+                    ("SPOT", "Spot", ""),
+                    ("AREA", "Area", ""),
+                ],
+                "name": "Type",
+            },
+        ),
+        ("energy", "float", {"name": "Energy", "default": 10.0}),
+        (
+            "color",
+            "vector",
+            {"name": "Color", "subtype": "COLOR", "default": (1, 1, 1)},
+        ),
+    ],
+)
--- nodes/scene_instance.py
+++ nodes/scene_instance.py
@@ -1,35 +1,48 @@
 import bpy
-from .base import (
-    BaseNode,
-    SceneSocket,
-    StringSocket,
-)
+from .base import BaseNode, build_props_and_sockets
 
 class SceneInstanceNode(BaseNode):
     bl_idname = "SceneInstanceNodeType"
     bl_label = "Scene Instance"
 
-    file_path: bpy.props.StringProperty(name="File Path", subtype='FILE_PATH')
-    collection_path: bpy.props.StringProperty(name="Collection Path")
-    load_mode: bpy.props.EnumProperty(
-        name="Load Mode",
-        items=[
-            ('APPEND', "Append", "Append the collection"),
-            ('INSTANCE', "Instance", "Append collection and create instance object"),
-            ('LINK', "Link", "Link the collection"),
-            ('OVERRIDE', "Link Override", "Link the collection and create a library override"),
-        ],
-        default='APPEND'
-    )
-
     def init(self, context):
-        fp = self.inputs.new('StringSocketType', "File Path")
-        fp.value = self.file_path
-        cp = self.inputs.new('StringSocketType', "Collection Path")
-        cp.value = self.collection_path
-        lm = self.inputs.new('StringSocketType', "Load Mode")
-        lm.value = self.load_mode
+        self.add_property_sockets()
         self.outputs.new('SceneSocketType', "Scene")
 
     def draw_buttons(self, context, layout):
         pass  # Inputs already expose editable values next to their sockets
+
+
+build_props_and_sockets(
+    SceneInstanceNode,
+    [
+        (
+            "file_path",
+            "string",
+            {"name": "File Path", "subtype": "FILE_PATH"},
+        ),
+        (
+            "collection_path",
+            "string",
+            {"name": "Collection Path"},
+        ),
+        (
+            "load_mode",
+            "enum",
+            {
+                "name": "Load Mode",
+                "items": [
+                    ("APPEND", "Append", "Append the collection"),
+                    ("INSTANCE", "Instance", "Append collection and create instance object"),
+                    ("LINK", "Link", "Link the collection"),
+                    (
+                        "OVERRIDE",
+                        "Link Override",
+                        "Link the collection and create a library override",
+                    ),
+                ],
+                "default": "APPEND",
+            },
+        ),
+    ],
+)
