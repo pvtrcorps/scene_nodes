83b983f91045871f2c7f39bfed8f54b0c885c084
pvtrcorps <antoniojaviertorralbamoreno@gmail.com>
2025-06-16 01:16:01 +0200

    Add socket visibility panel

--- ui/__init__.py
+++ ui/__init__.py
@@ -4,6 +4,7 @@ from .operators import NODE_OT_sync_to_scene
 from .node_panel import (
     SCENE_NODES_PT_node_props,
     SCENE_NODES_PT_node_props_properties,
+    SCENE_NODES_PT_socket_visibility,
 )
 
 __all__ = [
@@ -11,6 +12,7 @@ __all__ = [
     "NODE_OT_sync_to_scene",
     "SCENE_NODES_PT_node_props",
     "SCENE_NODES_PT_node_props_properties",
+    "SCENE_NODES_PT_socket_visibility",
 ]
 from . import node_editor
 from . import node_panel
