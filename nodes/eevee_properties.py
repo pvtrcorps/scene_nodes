d9509d628e3164940f93325d319d6ce45255ff70
pvtrcorps <antoniojaviertorralbamoreno@gmail.com>
2025-06-18 08:13:22 +0100

    Expand render property nodes

--- nodes/eevee_properties.py
+++ nodes/eevee_properties.py
@@ -18,14 +18,13 @@ class EeveePropertiesNode(BaseNode):
 build_props_and_sockets(
     EeveePropertiesNode,
     [
+        # General Output
         ("res_x", "int", {"name": "Resolution X", "default": 1920}),
         ("res_y", "int", {"name": "Resolution Y", "default": 1080}),
         ("frame_start", "int", {"name": "Frame Start", "default": 1}),
         ("frame_end", "int", {"name": "Frame End", "default": 250}),
         ("fps", "int", {"name": "FPS", "default": 24}),
         ("camera_path", "string", {"name": "Camera Path"}),
-        ("samples", "int", {"name": "Samples", "default": 64}),
-        ("motion_blur", "bool", {"name": "Motion Blur"}),
         ("filepath", "string", {"name": "File Path", "subtype": "FILE_PATH"}),
         (
             "file_format",
@@ -51,5 +50,23 @@ build_props_and_sockets(
                 "default": "RGB",
             },
         ),
+
+        # Sampling
+        ("samples", "int", {"name": "Samples", "default": 64}),
+        ("use_taa_reprojection", "bool", {"name": "TAA Reprojection"}),
+
+        # Clamping
+        ("clamp_direct", "float", {"name": "Clamp Direct", "default": 0.0}),
+        ("clamp_indirect", "float", {"name": "Clamp Indirect", "default": 0.0}),
+
+        # Raytracing
+        ("raytrace_resolution", "int", {"name": "Raytrace Resolution", "default": 512}),
+
+        # Motion Blur
+        ("motion_blur", "bool", {"name": "Motion Blur"}),
+        ("motion_blur_shutter", "float", {"name": "Shutter", "default": 0.5}),
+
+        # Film
+        ("film_exposure", "float", {"name": "Exposure", "default": 1.0}),
     ],
 )
